c9db6eeb29008ca43ac62eff8432ec86f815b96f
Jansel <janselfritz096@gmail.com>
2020-10-29 01:08:10 +0100

    begun writing, downloads do come later

--- Jansels-SE/Program.cs
+++ Jansels-SE/Program.cs
@@ -10,6 +10,13 @@ namespace Jansels_SE
     {
         static void Main(string[] args)
         {
+            Console.Title = "Jansel Software Enabler V1.0";
+            Console.CursorVisible = false;
+            Console.WriteLine("This Program enables Desktop and downloads Software like OBS Studio and Firefox");
+            string user = Environment.UserName;
+            Console.WriteLine("Current User (User that executed this): " + user);
+            Console.ReadKey();
+
         }
     }
 }
